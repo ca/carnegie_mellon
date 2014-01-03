f9ecd5c4da89383b5c1aa4f709394eb65eafa3a5
Alex Vallorosi <reddog9287@aim.com>
2014-01-03 14:26:28 -0500

    prevent people from voting large numbers of times on the frontend

--- pub/js/VoteCtrl.js
+++ pub/js/VoteCtrl.js
@@ -4,6 +4,8 @@ app.controller("VoteCtrl", function($scope, $http){
 
     $scope.result = {};
 
+    var count = 0;
+
     $http.get('/api/votes')
     .success(function(data, status, headers, config) {
         $scope.result = {
@@ -17,23 +19,33 @@ app.controller("VoteCtrl", function($scope, $http){
     });
 
 	$scope.voteUp = function () {
-        $scope.result.upvotes += 1;
-        $http.put('/api/votes/52c3b1b8ecd11f3d564076fe', $scope.result)
-        .success(function(data, status, headers, config) {
-            console.warn(data);
-        }).error(function(data, status, headers, config) {
-            console.log("error", data, status, headers, config);
-        });
+        count++;
+        if (count <= 10) {
+            $scope.result.upvotes += 1;
+            $http.put('/api/votes/52c3b1b8ecd11f3d564076fe', $scope.result)
+            .success(function(data, status, headers, config) {
+                console.warn(data);
+            }).error(function(data, status, headers, config) {
+                console.log("error", data, status, headers, config);
+            });
+        } else {
+            alert('You\'ve already voted ten times!');
+        }
 	}
 
 	$scope.voteDown = function () {
 		alert('Alright! You got me!');
-        $scope.result.downvotes += 1;
-        $http.put('/api/votes/52c3b1b8ecd11f3d564076fe', $scope.result)
-        .success(function(data, status, headers, config) {
-            console.warn(data);
-        }).error(function(data, status, headers, config) {
-            console.log("error", data, status, headers, config);
-        });
+        count++;
+        if (count <= 10) {
+            $scope.result.downvotes += 1;
+            $http.put('/api/votes/52c3b1b8ecd11f3d564076fe', $scope.result)
+            .success(function(data, status, headers, config) {
+                console.warn(data);
+            }).error(function(data, status, headers, config) {
+                console.log("error", data, status, headers, config);
+            });
+        } else {
+            alert('You\'ve already voted ten times!');
+        }
 	}
 });
